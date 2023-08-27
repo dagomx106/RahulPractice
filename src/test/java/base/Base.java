14e34bca490d0a3283eb9b74cb3a326de122cdc9
dagomx106 <dagovf@outlook.com>
2023-08-26 21:51:30 -0600

    adding docker and selenium grid options

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -5,37 +5,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 
 public class Base {
 
     public static WebDriver driver;
-    //DesiredCapabilities dc = new DesiredCapabilities();
+    public static DesiredCapabilities dc = new DesiredCapabilities();
 
-//    @BeforeClass
-//    public void setupGrid() throws MalformedURLException {
-//
-//        dc.setBrowserName("chrome");
-//        driver = new RemoteWebDriver(new URL("http://192.168.100.5:4444"),dc);
-//
-//        driver.manage().timeouts().implicitlyWait(ofSeconds(10));
-//
-//        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-//        driver.manage().window().maximize();
-//
-//    }
 @Parameters("browser")
 @BeforeClass
-    public static void setup(@Optional("chrome")String browser) {
+    public static void setup(@Optional("chrome")String browser) throws MalformedURLException {
     if (browser.equalsIgnoreCase("chrome")) {
         driver = new ChromeDriver();
     }else if (browser.equalsIgnoreCase("edge")) {
         driver = new EdgeDriver();
-    }else
+    }else if (browser.equalsIgnoreCase("grid")) {
+        dc.setBrowserName("MicrosoftEdge");
+        driver = new RemoteWebDriver(new URL("http://169.254.3.142:4444"),dc);
+    }else if (browser.equalsIgnoreCase("docker")) {
+        dc.setBrowserName("chrome");
+            driver = new RemoteWebDriver(new URL("http://192.168.100.56:4444"),dc);
+    } else
         driver = new FirefoxDriver();
+
 
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.manage().window().maximize();
